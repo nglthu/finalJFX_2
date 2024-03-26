9cc70613c329eea63f934efbff07e74d5aa168a1
Lethu Nguyen <lethunguyen@Lethus-MacBook-Pro.local>
2024-03-26 17:20:54 +0700

    read /write file

---
+++ src/fileReaderWriter.java
@@ -0,0 +1,34 @@
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+public class fileReaderWriter {
+
+    public static void main(String[] args) {
+        File f = new File("final/src/test.txt");
+        //File fw = new File("final/src/writeTest.txt");
+
+       
+
+        System.out.println(f.getName());
+        System.out.println(f.getPath());
+        try {
+            FileInputStream fis = new FileInputStream(f);
+            FileWriter fwa = new FileWriter("writeTest.txt");
+            int c;
+            while ((c = fis.read()) != -1) {
+                System.out.println((char) c);
+                
+               // fwa.write((char)c);
+                fwa.append((char)c);
+                fwa.write(c);
+                
+                //fwa.close();
+            }
+            fwa.close();
+            System.out.println("file is written");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
